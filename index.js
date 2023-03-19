cef26531ac3010146e1f1d96b9085eb6d6055e10
NyakundiMauya <n.mauya.nm@gmail.com>
2023-03-19 12:59:45 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+function divide(division=2000/100) {
+    // let division=
+   return division;
+}
+const square = (parameter1) => Math.pow(parameter1,2);
+const add = (parameter2, parameter3) => parameter2 + parameter3;
